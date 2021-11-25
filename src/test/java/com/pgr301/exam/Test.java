f95418c40737b5437a627efff2635dccace92892
cecilie <ceciversen@gmail.com>
2021-11-25 14:21:45 +0100

    Added simple test

---
+++ src/test/java/com/pgr301/exam/Test.java
@@ -0,0 +1,13 @@
+package com.pgr301.exam;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@SpringBootTest
+public class Test {
+
+    void testGetAccount(){
+        assertEquals(1, 1);
+    }
+}
