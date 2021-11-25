9f8228baaa678d89953cac7bacde80c3a94fbe2d
cecilie <ceciversen@gmail.com>
2021-11-25 14:59:59 +0100

    Try to break test

--- src/main/java/com/pgr301/exam/ReallyShakyBankingCoreSystemService.java
+++ src/main/java/com/pgr301/exam/ReallyShakyBankingCoreSystemService.java
@@ -63,7 +63,7 @@ class ReallyShakyBankingCoreSystemService implements BankingCoreSystmeService {
 
     private Account getOrCreateAccount(String accountId) {
         if (theBank.get(accountId) == null) {
-            Account a = new ccount();
+            Account a = new Account();
             a.setId(accountId);
             theBank.put(accountId, a);
         }
