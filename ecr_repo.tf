68b81716bbe4a589e97ccd311f34e7cd2f943c58
cecilie <ceciversen@gmail.com>
2021-11-26 16:15:05 +0100

    Removed iam user file

--- ecr_repo.tf
+++ ecr_repo.tf
@@ -1,8 +1,3 @@
 resource "aws_ecr_repository" "student_personal_repo" {
   name = var.student_id
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = false
-  }
 }
